bb2f347f4eb8f739345a96be7730e543b3c254de
Forestas <56953424+Yan420@users.noreply.github.com>
2020-03-17 12:08:43 +0100

    Add files via upload

---
+++ Chemin.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Chemin {
+	
+	private ArrayList<Arret> chemin;
+
+	public Chemin(ArrayList<Arret> chemin) {
+		super();
+		this.chemin = chemin;
+	}
+	
+	public ArrayList<Arret> getChemin() {
+		return chemin;
+	}
+}
